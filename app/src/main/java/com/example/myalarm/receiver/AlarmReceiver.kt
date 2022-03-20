86d1f5b278fee0be207c13d3d899a611d84079ad
namnt1997 <ntnamit97@gmail.com>
2022-03-20 07:55:15 +0700

    Add permission, remove log

--- app/src/main/java/com/example/myalarm/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/example/myalarm/receiver/AlarmReceiver.kt
@@ -3,13 +3,11 @@ package com.example.myalarm.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.example.myalarm.utilities.ListAlarmManager
 import com.example.myalarm.utilities.NotificationUseCase
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("AlarmReceiver", "onReceive")
         context?.let {
             NotificationUseCase.showAlarmNotification(it)
             ListAlarmManager.refreshAlarm(context.applicationContext)
--- app/src/main/java/com/example/myalarm/utilities/AlarmHelper.kt
+++ app/src/main/java/com/example/myalarm/utilities/AlarmHelper.kt
@@ -4,10 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.example.myalarm.receiver.AlarmReceiver
-import java.text.SimpleDateFormat
-import java.util.*
 
 object AlarmHelper {
 
@@ -15,11 +12,6 @@ object AlarmHelper {
     const val ACTION_AlARM = "ACTION_AlARM"
 
     fun createAlarm(context: Context, timeMs: Long) {
-        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.US)
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = timeMs
-        Log.d("AlarmHelper", "createAlarm at: ${sdf.format(calendar.time)}")
-
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         val intent = Intent(context, AlarmReceiver::class.java)
